0ff60597c53e2fd0b2ec7019d3edd3625f5f6db3
Mr-mpange <kilindosaid771@gmail.com>
2026-04-15 18:19:55 +0300

    fix: hardcode base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/tanzania-poultry-connect/" : "/",
+  base: "/tanzania-poultry-connect/",
   server: {
     host: "::",
     port: 8080,
